c64b8cb343fbbe0569b49562807ee3109deabd57
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-15 19:21:35 +0100

    Added loop

---
+++ JS/sript.js
@@ -0,0 +1,71 @@
+const app = new Vue({
+    el: "#root",
+    data:{
+        arrContacts: [
+            {
+                "contactImg": "avatar_1.jpg",
+                "contactData": {
+                    "contactName": "Michele",
+                    "lastMex": "Ultimo messaggio inviato"
+                },
+                "lastLog": "12:00"
+            },
+            {
+                "contactImg": "avatar_2.jpg",
+                "contactData": {
+                    "contactName": "Fabio",
+                    "lastMex": "Ultimo messaggio inviato"
+                },
+                "lastLog": "12:00"
+            },
+            {
+                "contactImg": "avatar_3.jpg",
+                "contactData": {
+                    "contactName": "Samuele",
+                    "lastMex": "Ultimo messaggio inviato"
+                },
+                "lastLog": "12:00"
+            },
+            {
+                "contactImg": "avatar_4.jpg",
+                "contactData": {
+                    "contactName": "Alessandro B.",
+                    "lastMex": "Ultimo messaggio inviato"
+                },
+                "lastLog": "12:00"
+            },
+            {
+                "contactImg": "avatar_5.jpg",
+                "contactData": {
+                    "contactName": "Alessandro L.",
+                    "lastMex": "Ultimo messaggio inviato"
+                },
+                "lastLog": "12:00"
+            },
+            {
+                "contactImg": "avatar_6.jpg",
+                "contactData": {
+                    "contactName": "Claudia",
+                    "lastMex": "Ultimo messaggio inviato"
+                },
+                "lastLog": "12:00"
+            },
+            {
+                "contactImg": "avatar_7.jpg",
+                "contactData": {
+                    "contactName": "Federico",
+                    "lastMex": "Ultimo messaggio inviato"
+                },
+                "lastLog": "12:00"
+            },
+            {
+                "contactImg": "avatar_8.jpg",
+                "contactData": {
+                    "contactName": "Davide",
+                    "lastMex": "Ultimo messaggio inviato"
+                },
+                "lastLog": "12:00"
+            },
+        ]
+    }
+})
